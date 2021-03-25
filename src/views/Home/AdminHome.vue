2de4ab716322af61bd1b40c331015fefb01fb88e
MoMaek9 <1308994506@qq.com>
2021-03-25 11:29:48 +0800

    3.25 adminhome

--- src/views/Home/AdminHome.vue
+++ src/views/Home/AdminHome.vue
@@ -2,45 +2,18 @@
   <div id="admin-home">
     <div class="header"></div>
     <div class="content">
-      <el-tabs v-model="tabActive" @tab-click="handleClick">
-        <el-tab-pane label="注册管理" name="first">
+      <el-tabs v-model="tabActive">
+        <el-tab-pane label="账号管理" name="first">
           <div class="item-reg">
             <el-table :data="regUsers" style="width: 100%">
               <el-table-column prop="createdOn" label="日期" width="180"></el-table-column>
               <el-table-column prop="name" label="姓名" width="180"></el-table-column>
               <el-table-column prop="email" label="邮箱"></el-table-column>
-              <el-table-column label="是否允许">
+              <el-table-column prop="userStatus" label="账号状态"></el-table-column>
+              <el-table-column label="账号操作">
                 <template slot-scope="scope">
-                  <el-button type="primary" @click="allowReg(scope.row.id)" @click.native.prevent="deleteRow(scope.$index, regUsers)">允许</el-button>
-                  <el-button type="danger" @click="refuseReg(scope.row.id)" @click.native.prevent="deleteRow(scope.$index, regUsers)">拒绝</el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-          </div>
-        </el-tab-pane>
-        <el-tab-pane label="权限管理" name="second">
-          <div class="item-autho">
-            <el-table :data="userList" style="width: 100%">
-              <el-table-column label="头像" width="180">
-                <template slot-scope="scope">
-                  <el-image :src="scope.row.photoUrl" width="150" fit="contain"></el-image>
-                </template>
-              </el-table-column>
-              <el-table-column prop="name" label="名称" width="180"></el-table-column>
-              <el-table-column prop="biography" label="介绍" width="180"></el-table-column>
-              <el-table-column prop="email" label="邮箱"></el-table-column>
-              <el-table-column label="给予权限" width="180">
-                <template slot-scope="scope">
-                  <el-dropdown>
-                    <el-button type="primary">
-                      权限<i class="el-icon-arrow-down el-icon--right"></i>
-                    </el-button>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>审核商品</el-dropdown-item>
-                      <el-dropdown-item>审核注册</el-dropdown-item>
-                      <el-dropdown-item>全部</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
+                  <el-button type="primary" @click="banUser(scope.row.id)" @click.native.prevent="deleteRow(scope.$index, regUsers)">封禁</el-button>
+                  <el-button type="danger" @click="unbanUser(scope.row.id)" @click.native.prevent="deleteRow(scope.$index, regUsers)">解封</el-button>
                 </template>
               </el-table-column>
             </el-table>
@@ -76,47 +49,46 @@ export default {
   data () {
     return {
       tabActive: 'first',
-      regUsers: [{ id: 0, name: '', createOn: '', email: '' }],
+      allUsers: [{ id: 0, name: '', createOn: '', email: '' }],
       auditCommos: [],
       userList: []
     }
   },
   methods: {
-    handleClick (tab, event) {
+    //更改为qs插件形式
+    // allowReg (id) {
+    //   this.$axios.put('/apis/users/' + id, {
+    //     role: 0
+    //   }).then(resp => {
+    //     var data = resp.data
+    //     if (data.code === 200) {
+    //       this.$notify({
+    //         title: '成功',
+    //         message: '处理完成',
+    //         type: 'success'
+    //       })
+    //     }
+    //   }).catch(function (error) {
+    //     console.log(error)
+    //   })
+    // },
+    // refuseReg (id) {
+    //   this.$axios.put('/apis/users/' + id, {
+    //     role: -1
+    //   }).then(resp => {
+    //     var data = resp.data
+    //     if (data.code === 200) {
+    //       this.$notify({
+    //         title: '成功',
+    //         message: '处理完成',
+    //         type: 'success'
+    //       })
+    //     }
+    //   }).catch(function (error) {
+    //     console.log(error)
+    //   })
+    // },
 
-    },
-    allowReg (id, index, regUsers) {
-      this.$axios.put('/apis/users/' + id, {
-        role: 0
-      }).then(resp => {
-        var data = resp.data
-        if (data.code === 200) {
-          this.$notify({
-            title: '成功',
-            message: '处理完成',
-            type: 'success'
-          })
-        }
-      }).catch(function (error) {
-        console.log(error)
-      })
-    },
-    refuseReg (id) {
-      this.$axios.put('/apis/users/' + id, {
-        role: -1
-      }).then(resp => {
-        var data = resp.data
-        if (data.code === 200) {
-          this.$notify({
-            title: '成功',
-            message: '处理完成',
-            type: 'success'
-          })
-        }
-      }).catch(function (error) {
-        console.log(error)
-      })
-    },
     deleteRow (index, rows) {
       rows.splice(index, 1)
     },
@@ -160,6 +132,7 @@ export default {
         this.regUsers = data.data
       }
     })
+    //待审核商品列表
     this.$axios.get('/apis/commodities/audit').then(resp => {
       var data = resp.data
       if (data.code === 200) {
@@ -176,7 +149,7 @@ export default {
     background: rgb(70, 70, 70);
   }
   .content {
-    width: 900px;
+    width: 90%;
     margin: 30px auto;
   }
 }
