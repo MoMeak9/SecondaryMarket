e47b6746ce421c612c68f99262b7695984005f5d
MoMaek9 <1308994506@qq.com>
2021-03-26 17:09:32 +0800

    3.26 login token更改

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,20 @@ Vue.use(Vuex)
 //更换为Token存储
 const state = {
   user: window.sessionStorage.getItem('user'),
-  commodity: window.sessionStorage.getItem('commodity')
+  commodity: window.sessionStorage.getItem('commodity'),
+  hostServer:'',
+  token:''
 }
 const mutations = {
+
+  SET_TOKEN: (state, token)=>{
+    state.token = token
+    sessionStorage.token = token
+  },
+  DEL_TOKEN:(state)=>{
+    state.token = ''
+    sessionStorage.removeItem('token')
+  },
   GET_USER: (state, data) => {
     // 把用户名存起来
     state.user = data
